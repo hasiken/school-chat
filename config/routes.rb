4a1a9801374900328e5d4230ddc877a43107df44
hasiken <kentarou.123.99@gmail.com>
2020-04-18 12:43:23 +0900

    group,user_groupの生成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :groups, only:[:index, :new,:create,:edit,:update]
   resources :messages, only: [:index,:create]
 end
