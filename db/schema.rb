fa47a5abec9fdd46c34742872e9bff0587868e9c
hasiken <kentarou.123.99@gmail.com>
2020-04-28 00:42:25 +0900

    text-fieldcss

--- db/schema.rb
+++ db/schema.rb
@@ -31,8 +31,12 @@ ActiveRecord::Schema.define(version: 2020_04_18_033312) do
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.string "image"
+    t.integer "user_id"
+    t.integer "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_messages_on_group_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
